e2465a2d20b96e3ebc398b5e9ff48b7102cb30d0
amalrajesh01 <amalrajesh01@gmail.com>
2024-01-19 02:52:56 +0530

    bug fixed

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -21,15 +21,7 @@ export class LandingComponent {
 
   async getGeolocation() {
     try {
-      const response = await axios.get('http://ip-api.com/json');
-      this.geolocation = response.data;
-      console.warn(response.data);
       this.userInfo = {
-        Ip: response.data.query,
-        Country: response.data.country,
-        Region: response.data.regionName,
-        city: response.data.city,
-        isp: response.data.isp,
         isMobile: this.getDeviceType(),
         screenResolution: `${window.screen.width}x${window.screen.height}`,
         browser: navigator.userAgent,
@@ -41,12 +33,6 @@ export class LandingComponent {
         language: navigator.language || 'N/A',
         onlineStatus: navigator.onLine ? 'Online' : 'Offline',
         doNotTrack: navigator.doNotTrack || 'N/A',
-        geolocationCoordslat: response.data.lat,
-        geolocationCoordslon: response.data.lon,
-        ispOrg: response.data.org,
-        AutonomousSystemNumber: response.data.as,
-
-
       };
     } catch (error) {
       console.error('Error fetching geolocation:', error);
@@ -66,13 +52,6 @@ export class LandingComponent {
 
   getUserInfoItems(): UserInfoItem[] {
     return [
-      { label: 'IP Address', value: this.userInfo.Ip },
-      { label: 'Country', value: this.userInfo.Country },
-      { label: 'Region', value: this.userInfo.Region },
-      { label: 'City', value: this.userInfo.city },
-      { label: 'Internet Service Provider', value: this.userInfo.isp },
-      { label: 'Internet Service Provider Company', value: this.userInfo.ispOrg },
-      { label: 'Autonomous System Number (ASN)', value: this.userInfo.AutonomousSystemNumber },
       { label: 'Device Type', value: this.userInfo.isMobile },
       { label: 'Screen Resolution', value: this.userInfo.screenResolution },
       { label: 'Browser', value: this.userInfo.browser },
@@ -82,10 +61,6 @@ export class LandingComponent {
       { label: 'Device Memory', value: this.userInfo.deviceMemory },
       { label: 'Cookies Enabled', value: this.userInfo.cookiesEnabled },
       { label: 'Language', value: this.userInfo.language },
-      { label: 'Online Status', value: this.userInfo.onlineStatus },
-      { label: 'Do Not Track', value: this.userInfo.doNotTrack },
-      { label: 'Geolocation Coordinates Latitude', value: this.userInfo.geolocationCoordslat },
-      { label: 'Geolocation Coordinates longitude', value: this.userInfo.geolocationCoordslon },
 
     ];
   }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBkQB8tVVaHb5xEVAhYqTMUD8ftA-qyPSI",
+  authDomain: "kattu-in.firebaseapp.com",
+  projectId: "kattu-in",
+  storageBucket: "kattu-in.appspot.com",
+  messagingSenderId: "1095224424192",
+  appId: "1:1095224424192:web:3b132fa3fb29a36bc2e5e5",
+  measurementId: "G-3XZN5TYW5W"
+
+  },
+  production: true
+};
